a7aef650468b59de1b6bf3db0b5d5f6f6ff4f28a
jinyung0101java2 <jinyung0101java2@gmail.com>
2024-04-01 10:30:01 +0900

    Add coredns-apply.sh

---
+++ coredns-apply.sh
@@ -0,0 +1,38 @@
+# coredns configmap 수정 
+## 'localdomain' 추가
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ConfigMap
+metadata:
+  name: coredns
+  namespace: kube-system
+data:
+  Corefile: |
+    .:53 {
+        errors
+        health {
+           lameduck 5s
+        }
+        ready
+        kubernetes cluster.local localdomain in-addr.arpa ip6.arpa {
+           pods insecure
+           fallthrough in-addr.arpa ip6.arpa
+           ttl 30
+        }
+        prometheus :9153
+        hosts {
+           192.168.49.1 host.minikube.internal
+           fallthrough
+        }
+        forward . /etc/resolv.conf {
+           max_concurrent 1000
+        }
+        cache 30
+        loop
+        reload
+        loadbalance
+    }
+EOF
+
+# coredns deployment 재시작
+kubectl rollout restart deployment coredns  -n kube-system
